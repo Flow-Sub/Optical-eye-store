74f20838ea2abdd03205e3bda72daedb5d991557
Flow-Sub <f190240@nu.edu.pk>
2025-11-10 01:44:06 +0500

    updated

--- src/types/calendly.d.ts
+++ src/types/calendly.d.ts
@@ -1,7 +1,36 @@
 declare global {
   interface Window {
     Calendly: {
-      initPopupWidget: (options: { url: string }) => void;
+      initPopupWidget: (options: {
+        url: string;
+        prefill?: {
+          name?: string;
+          email?: string;
+          firstName?: string;
+          lastName?: string;
+          customAnswers?: {
+            a1?: string;
+            a2?: string;
+            a3?: string;
+            a4?: string;
+            a5?: string;
+            a6?: string;
+            a7?: string;
+            a8?: string;
+            a9?: string;
+            a10?: string;
+          };
+        };
+        utm?: {
+          utmCampaign?: string;
+          utmSource?: string;
+          utmMedium?: string;
+          utmContent?: string;
+          utmTerm?: string;
+        };
+      }) => void;
+      closePopupWidget?: () => void;
+      showPopupWidget?: (url: string) => void;
     };
   }
 }
